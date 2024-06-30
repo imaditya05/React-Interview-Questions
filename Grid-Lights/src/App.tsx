8be93a7e27d2bbf2a7a5e63da0c2b58cd3c22839
imaditya05 <imaditya05@gmail.com>
2024-06-30 23:47:17 +0530

    Grid-lights || Problem solved

--- Grid-Lights/src/App.tsx
+++ Grid-Lights/src/App.tsx
@@ -1,16 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.tsx
+import "./index.css";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div>
-      Grid Lights
-    </div>
-  )
+interface CellProps {
+  filled: boolean;
+  onClick: () => void;
+  isDisabled: boolean;
+  label: string;
 }
 
-export default App
+function Cell({ filled, onClick, isDisabled, label }: CellProps) {
+  return (
+    <button
+      type="button"
+      aria-label={label}
+      disabled={isDisabled}
+      onClick={onClick}
+      className={filled ? "cell cell-activated" : "cell"}
+    />
+  );
+}
+
+export default function App() {
+  const [order, setOrder] = useState<number[]>([]);
+  const [isDeactivating, setIsDeactivating] = useState<boolean>(false);
+
+  const config = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ];
+
+  const deactivateCells = () => {
+    setIsDeactivating(true);
+    const timer = setInterval(() => {
+      setOrder((origOrder) => {
+        const newOrder = origOrder.slice();
+        newOrder.pop();
+
+        if (newOrder.length === 0) {
+          clearInterval(timer);
+          setIsDeactivating(false);
+        }
+
+        return newOrder;
+      });
+    }, 300);
+  };
+
+  const activateCells = (index: number) => {
+    const newOrder = [...order, index];
+    setOrder(newOrder);
+    // deactivate
+    if (newOrder.length === config.flat(1).filter(Boolean).length) {
+      deactivateCells();
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div
+        className="grid"
+        style={{
+          gridTemplateColumns: `repeat(${config[0].length}, 1fr)`,
+        }}
+      >
+        {config.flat(1).map((value, index) => {
+          return value ? (
+            <Cell
+              key={index}
+              label={`Cell ${index}`}
+              filled={order.includes(index)}
+              onClick={() => activateCells(index)}
+              isDisabled={order.includes(index) || isDeactivating}
+            />
+          ) : (
+            <span key={index} />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
